e191198ec21462c1383d6ef9d4fc5906af70f1a2
Dale Andrijauskas <dale@hmdtrucking.com>
2025-05-05 19:22:18 -0600

    Cleanup, structure ready

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-container class="fill-height d-flex flex-column justify-center align-center text-center">
+    <v-row>
+      <v-col>
+        <h1 class="text-h2 font-weight-bold mb-4">WiFi Radar</h1>
+        <p class="text-subtitle-1 mb-6">
+          Explore and discover free public Wi-Fi hotspots near you.
+        </p>
+        <v-btn color="primary" large @click="goToMap">Start Exploring</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const goToMap = () => router.push('/map')
+</script>
