bc0e5da28eaf078ed144901468726bc0dc52799a
paplauskis <paplauskaskostas@gmail.com>
2025-05-06 19:18:39 +0300

    create backend

---
+++ backend/API/Controllers/WifiController.cs
@@ -0,0 +1,13 @@
+namespace API.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class WifiController
+{
+    public WifiController()
+    {
+        
+    }
+}
