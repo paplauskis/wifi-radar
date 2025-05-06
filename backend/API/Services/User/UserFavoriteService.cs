74e8d7cc80581b321def8f2507682788da9c08c7
paplauskis <paplauskaskostas@gmail.com>
2025-05-06 19:37:18 +0300

    create service class

---
+++ backend/API/Services/User/UserFavoriteService.cs
@@ -0,0 +1,11 @@
+using API.Services.Interfaces.User;
+
+namespace API.Services.User;
+
+public class UserFavoriteService : IUserFavoriteService
+{
+    public UserFavoriteService()
+    {
+        
+    }
+}
---
+++ backend/API/Services/Wifi/WifiSearchService.cs
@@ -0,0 +1,10 @@
+using API.Services.Interfaces.Wifi;
+
+namespace API.Services.Wifi;
+
+public class WifiSearchService : IWifiSearchService
+{
+    public WifiSearchService()
+    {
+    }
+}
