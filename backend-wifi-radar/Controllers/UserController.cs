dc613f99878d80cc17484bf71c8756f457599c46
paplauskis <paplauskaskostas@gmail.com>
2025-05-06 11:52:34 +0300

    add backend
    created rough structure of the backend

---
+++ backend-wifi-radar/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_wifi_radar.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class UserController : ControllerBase
+{
+    public UserController()
+    {
+        
+    }
+}
---
+++ backend-wifi-radar/Controllers/WifiController.cs
@@ -0,0 +1,13 @@
+namespace backend_wifi_radar.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class WifiController
+{
+    public WifiController()
+    {
+        
+    }
+}
