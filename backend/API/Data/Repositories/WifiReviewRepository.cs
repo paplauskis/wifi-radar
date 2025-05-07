c65907b755169e4cad6f5e29ec24ca8b2f91d8f3
paplauskis <paplauskaskostas@gmail.com>
2025-05-07 12:48:40 +0300

    create repo class to get WifiReview data from DB
    inherits BaseRepository

---
+++ backend/API/Data/Repositories/WifiReviewRepository.cs
@@ -0,0 +1,10 @@
+using API.Domain.Models;
+
+namespace API.Data.Repositories;
+
+public class WifiReviewRepository : BaseRepository<WifiReview>
+{
+    public WifiReviewRepository()
+    {
+    }
+}
