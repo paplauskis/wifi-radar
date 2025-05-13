8458e4f2a56878b081d2a72f934161ea562a7766
paplauskis <paplauskaskostas@gmail.com>
2025-05-13 16:36:13 +0300

    added IWifiRepository, removed redundant GetByIdAsync method

--- backend/API/Data/Repositories/WifiRepository.cs
+++ backend/API/Data/Repositories/WifiRepository.cs
@@ -1,9 +1,10 @@
+using API.Data.Repositories.Interfaces;
 using API.Domain.Models;
 using MongoDB.Driver;
 
 namespace API.Data.Repositories;
 
-public class WifiRepository : BaseRepository<Wifi>
+public class WifiRepository : BaseRepository<Wifi>, IWifiRepository
 {
     public WifiRepository(IMongoDatabase database) : base (database, "Favorite-WiFi-Spots")
     {
@@ -13,11 +14,5 @@ public class WifiRepository : BaseRepository<Wifi>
     {
         var filter = Builders<Wifi>.Filter.Eq(w => w.City, city);
         return await _collection.Find(filter).ToListAsync();
-    }
-
-    public async Task<Wifi?> GetByIdAsync (string id)
-    {
-        var filter = Builders<Wifi>.Filter.Eq(w => w.Id, id);
-        return await _collection.Find(filter).FirstOrDefaultAsync();
     }
 }
