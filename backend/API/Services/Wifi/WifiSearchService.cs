43bce82ef680e9d5561f0686bc0d4233ebb755e3
paplauskis <paplauskaskostas@gmail.com>
2025-05-13 19:26:06 +0300

    added IWifiRepository via DI

--- backend/API/Services/Wifi/WifiSearchService.cs
+++ backend/API/Services/Wifi/WifiSearchService.cs
@@ -1,10 +1,20 @@
+using API.Data.Repositories.Interfaces;
 using API.Services.Interfaces.Wifi;
+using API.Domain.Models;
 
 namespace API.Services.Wifi;
 
 public class WifiSearchService : IWifiSearchService
 {
-    public WifiSearchService()
+    private readonly IWifiRepository _wifiRepository;
+    
+    public WifiSearchService(IWifiRepository repo)
     {
+        _wifiRepository = repo;
+    }
+
+    public async Task<List<WifiNetwork>> GetWifi()
+    {
+        throw new NotImplementedException();
     }
 }
