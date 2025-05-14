0d38e336ea852d0176b12474852cd135ad5661f4
paplauskis <paplauskaskostas@gmail.com>
2025-05-14 19:17:24 +0300

    added try...finally blocks
    to ensure context is disposed and no memory leaks occur

--- backend/Tests/RepositoryTests/WifiRepositoryTests.cs
+++ backend/Tests/RepositoryTests/WifiRepositoryTests.cs
@@ -25,54 +25,64 @@ public class WifiRepositoryTests : BaseRepositoryTests<WifiNetwork, WifiReposito
     [InlineData(9)]
     public override async Task AddAsync_ShouldInsertEntityIntoCollection(int num)
     {
-        _context = new TestDbContext(CollectionName);
-        _repo = GetRepository(_context);
-        _collection = _context.Database.GetCollection<WifiNetwork>(CollectionName);
+        try
+        {
+            _context = new TestDbContext(CollectionName);
+            _repo = GetRepository(_context);
+            _collection = _context.Database.GetCollection<WifiNetwork>(CollectionName);
 
-        var insertedEntity = await _repo.AddAsync(_testData[num]);
+            var insertedEntity = await _repo.AddAsync(_testData[num]);
 
-        var fetchedEntity = await _collection
-            .Find(e => e.Id == insertedEntity.Id)
-            .FirstOrDefaultAsync();
+            var fetchedEntity = await _collection
+                .Find(e => e.Id == insertedEntity.Id)
+                .FirstOrDefaultAsync();
         
-        Assert.Equal(insertedEntity.Id, fetchedEntity.Id);
-        Assert.Equal(insertedEntity.UserId, fetchedEntity.UserId);
-        Assert.Equal(insertedEntity.Country, fetchedEntity.Country);
-        Assert.Equal(insertedEntity.City, fetchedEntity.City);
-        Assert.Equal(insertedEntity.PlaceName, fetchedEntity.PlaceName);
-        Assert.Equal(insertedEntity.Street, fetchedEntity.Street);
-        Assert.Equal(insertedEntity.BuildingNumber, fetchedEntity.BuildingNumber);
-        Assert.Equal(insertedEntity.PostalCode, fetchedEntity.PostalCode);
-        Assert.Equal(insertedEntity.IsFree, fetchedEntity.IsFree);
-        Assert.Equal(insertedEntity.Password, fetchedEntity.Password);
-        
-        _context.Dispose();
+            Assert.Equal(insertedEntity.Id, fetchedEntity.Id);
+            Assert.Equal(insertedEntity.UserId, fetchedEntity.UserId);
+            Assert.Equal(insertedEntity.Country, fetchedEntity.Country);
+            Assert.Equal(insertedEntity.City, fetchedEntity.City);
+            Assert.Equal(insertedEntity.PlaceName, fetchedEntity.PlaceName);
+            Assert.Equal(insertedEntity.Street, fetchedEntity.Street);
+            Assert.Equal(insertedEntity.BuildingNumber, fetchedEntity.BuildingNumber);
+            Assert.Equal(insertedEntity.PostalCode, fetchedEntity.PostalCode);
+            Assert.Equal(insertedEntity.IsFree, fetchedEntity.IsFree);
+            Assert.Equal(insertedEntity.Password, fetchedEntity.Password);
+        }
+        finally
+        {
+            _context?.Dispose();
+        }
     }
 
     [Theory]
     [MemberData(nameof(ValidObjects))]
     public override async Task GetByIdAsync_ShouldReturnCorrectEntity_WhenIdExists(WifiNetwork entity)
     {
-        _context = new TestDbContext(CollectionName);
-        _repo = GetRepository(_context);
-        _collection = _context.Database.GetCollection<WifiNetwork>(CollectionName);
+        try
+        {
+            _context = new TestDbContext(CollectionName);
+            _repo = GetRepository(_context);
+            _collection = _context.Database.GetCollection<WifiNetwork>(CollectionName);
 
-        var insertedEntity = await _repo.AddAsync(entity);
-        var fetchedEntity = await _repo.GetByIdAsync(entity.Id);
+            var insertedEntity = await _repo.AddAsync(entity);
+            var fetchedEntity = await _repo.GetByIdAsync(entity.Id);
 
-        Assert.NotNull(fetchedEntity);
-        Assert.Equal(insertedEntity.Id, fetchedEntity.Id);
-        Assert.Equal(insertedEntity.UserId, fetchedEntity.UserId);
-        Assert.Equal(insertedEntity.Country, fetchedEntity.Country);
-        Assert.Equal(insertedEntity.City, fetchedEntity.City);
-        Assert.Equal(insertedEntity.PlaceName, fetchedEntity.PlaceName);
-        Assert.Equal(insertedEntity.Street, fetchedEntity.Street);
-        Assert.Equal(insertedEntity.BuildingNumber, fetchedEntity.BuildingNumber);
-        Assert.Equal(insertedEntity.PostalCode, fetchedEntity.PostalCode);
-        Assert.Equal(insertedEntity.IsFree, fetchedEntity.IsFree);
-        Assert.Equal(insertedEntity.Password, fetchedEntity.Password);
-        
-        _context.Dispose();
+            Assert.NotNull(fetchedEntity);
+            Assert.Equal(insertedEntity.Id, fetchedEntity.Id);
+            Assert.Equal(insertedEntity.UserId, fetchedEntity.UserId);
+            Assert.Equal(insertedEntity.Country, fetchedEntity.Country);
+            Assert.Equal(insertedEntity.City, fetchedEntity.City);
+            Assert.Equal(insertedEntity.PlaceName, fetchedEntity.PlaceName);
+            Assert.Equal(insertedEntity.Street, fetchedEntity.Street);
+            Assert.Equal(insertedEntity.BuildingNumber, fetchedEntity.BuildingNumber);
+            Assert.Equal(insertedEntity.PostalCode, fetchedEntity.PostalCode);
+            Assert.Equal(insertedEntity.IsFree, fetchedEntity.IsFree);
+            Assert.Equal(insertedEntity.Password, fetchedEntity.Password);
+        }
+        finally
+        {
+            _context?.Dispose();
+        }
     }
 
     protected override WifiRepository GetRepository(TestDbContext context)
