0affb0c4a73038a7d14a43fbad644c5fa427446b
paplauskis <paplauskaskostas@gmail.com>
2025-05-14 17:09:29 +0300

    set up test class

--- backend/Tests/RepositoryTests/WifiReviewRepositoryTests.cs
+++ backend/Tests/RepositoryTests/WifiReviewRepositoryTests.cs
@@ -1,9 +1,32 @@
+using API.Data.Repositories;
+using API.Domain.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
 namespace Tests.RepositoryTests;
 
-public class WifiReviewRepositoryTests
+public class WifiReviewRepositoryTests: BaseRepositoryTests<WifiReview, WifiReviewRepository>
 {
-    [Fact]
-    public void Test1()
+    private const string CollectionName = "Reviews";
+    
+    public WifiReviewRepositoryTests() : base(CollectionName) {}
+
+    public override Task AddAsync_ShouldInsertEntityIntoCollection(int num)
     {
+        throw new NotImplementedException();
+    }
+
+    public override Task GetByIdAsync_ShouldReturnCorrectEntity_WhenIdExists(WifiReview entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override WifiReviewRepository GetRepository(TestDbContext context)
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton(context.Database);
+        services.AddScoped<WifiReviewRepository>();
+        var serviceProvider = services.BuildServiceProvider();
+        return serviceProvider.GetRequiredService<WifiReviewRepository>();
     }
 }
