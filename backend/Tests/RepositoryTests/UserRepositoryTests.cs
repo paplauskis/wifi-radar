1a5d1979de15e6f0edeb04c2f44a01fe2244c079
paplauskis <paplauskaskostas@gmail.com>
2025-05-14 18:33:51 +0300

    implement AddAsync_ShouldInsertEntityIntoCollection test
    test checks if all added values to DB are valid

--- backend/Tests/RepositoryTests/UserRepositoryTests.cs
+++ backend/Tests/RepositoryTests/UserRepositoryTests.cs
@@ -1,6 +1,7 @@
 using API.Data.Repositories;
 using API.Domain.Models;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 using Xunit;
 
 namespace Tests.RepositoryTests;
@@ -12,9 +13,34 @@ public class UserRepositoryTests : BaseRepositoryTests<User, UserRepository>
     public UserRepositoryTests() : base(CollectionName) {}
 
 
-    public override Task AddAsync_ShouldInsertEntityIntoCollection(int num)
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(6)]
+    [InlineData(7)]
+    [InlineData(8)]
+    [InlineData(9)]
+    public override async Task AddAsync_ShouldInsertEntityIntoCollection(int num)
     {
-        throw new NotImplementedException();
+        _context = new TestDbContext(CollectionName);
+        _repo = GetRepository(_context);
+        _collection = _context.Database.GetCollection<User>(CollectionName);
+
+        var insertedEntity = await _repo.AddAsync(_testData[num]);
+
+        var fetchedEntity = await _collection
+            .Find(e => e.Id == insertedEntity.Id)
+            .FirstOrDefaultAsync();
+        
+        Assert.Equal(insertedEntity.Id, fetchedEntity.Id);
+        Assert.Equal(insertedEntity.Username, fetchedEntity.Username);
+        Assert.Equal(insertedEntity.Password, fetchedEntity.Password);
+        
+        _context.Dispose();
     }
 
     public override Task GetByIdAsync_ShouldReturnCorrectEntity_WhenIdExists(User entity)
