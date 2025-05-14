bb325e5cfc1e535e035c24e2757d396287f53169
paplauskis <paplauskaskostas@gmail.com>
2025-05-14 14:18:40 +0300

    created method to set up repository, added testData field

--- backend/Tests/RepositoryTests/WifiRepositoryTests.cs
+++ backend/Tests/RepositoryTests/WifiRepositoryTests.cs
@@ -1,9 +1,34 @@
+using API.Data.Repositories;
+using API.Domain.Models;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Tests.Helpers;
+using Xunit;
+
 namespace Tests.RepositoryTests;
 
 public class WifiRepositoryTests
 {
-    [Fact]
-    public void Test1()
+    private const string CollectionName = "Favorite-WiFi-Spots";
+    private readonly List<WifiNetwork> _testData = JsonHelper.GetPocoObjects<WifiNetwork>();
+    
+    public WifiRepositoryTests()
     {
+    }
+    
+    [Fact]
+    public async Task AddAsync_ShouldInsertEntityIntoCollection()
+    {
+        
+    }
+
+    private WifiRepository GetRepository(TestDbContext context)
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton(context.Database);
+        services.AddScoped<WifiRepository>();
+        var serviceProvider = services.BuildServiceProvider();
+        return serviceProvider.GetRequiredService<WifiRepository>();
     }
 }
