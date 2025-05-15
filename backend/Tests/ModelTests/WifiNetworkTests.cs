35d75be92a94a11e3f41a5e14683aedd9a740adf
paplauskis <paplauskaskostas@gmail.com>
2025-05-15 14:59:55 +0300

    changed namespaces

--- backend/Tests/ModelTests/WifiNetworkTests.cs
+++ backend/Tests/ModelTests/WifiNetworkTests.cs
@@ -8,28 +8,28 @@ namespace Tests.ModelTests;
 public class WifiNetworkTests
 {
     [Theory]
-    [MemberData(nameof(PropertyHelper.GetNonNullableStringProperties), MemberType = typeof(PropertyHelper))]
+    [MemberData(nameof(WifiNetworkPropertyHelper.GetNonNullableStringProperties), MemberType = typeof(WifiNetworkPropertyHelper))]
     public void NonNullableStringProperty_ShouldThrowArgumentException_WhenIsEmptyOrWhitespace(PropertyInfo property, string value)
     {
         AssertProperty(property, value);
     }
     
     [Theory]
-    [MemberData(nameof(PropertyHelper.GetNullableStringProperties), MemberType = typeof(PropertyHelper))]
+    [MemberData(nameof(WifiNetworkPropertyHelper.GetNullableStringProperties), MemberType = typeof(WifiNetworkPropertyHelper))]
     public void NullableStringProperty_ShouldThrowArgumentException_WhenIsEmptyOrWhitespace(PropertyInfo property, string value)
     {
         AssertProperty(property, value);
     }
     
     [Theory]
-    [MemberData(nameof(PropertyHelper.GetNonNullableIntProperties), MemberType = typeof(PropertyHelper))]
+    [MemberData(nameof(WifiNetworkPropertyHelper.GetNonNullableIntProperties), MemberType = typeof(WifiNetworkPropertyHelper))]
     public void NonNullableIntProperty_ShouldThrowArgumentException_WhenValueIsInvalid(PropertyInfo property, int value)
     {
         AssertProperty(property, value);
     }
     
     [Theory]
-    [MemberData(nameof(PropertyHelper.GetNullableIntProperties), MemberType = typeof(PropertyHelper))]
+    [MemberData(nameof(WifiNetworkPropertyHelper.GetNullableIntProperties), MemberType = typeof(WifiNetworkPropertyHelper))]
     public void NullableIntProperty_ShouldThrowArgumentException_WhenValueIsInvalid(PropertyInfo property, int? value)
     {
         AssertProperty(property, value);
