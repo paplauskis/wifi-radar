0cc512fedb8a2690ed0d7ba2a0b6efe9c36533cd
paplauskis <paplauskaskostas@gmail.com>
2025-05-15 14:40:01 +0300

    created private method AssertProperty to reduce repetitive code

--- backend/Tests/ModelTests/WifiNetworkTests.cs
+++ backend/Tests/ModelTests/WifiNetworkTests.cs
@@ -11,38 +11,31 @@ public class WifiNetworkTests
     [MemberData(nameof(PropertyHelper.GetNonNullableStringProperties), MemberType = typeof(PropertyHelper))]
     public void NonNullableStringProperty_ShouldThrowArgumentException_WhenIsEmptyOrWhitespace(PropertyInfo property, string value)
     {
-        var wifi = new WifiNetwork();
-        
-        var exception = Assert.Throws<ArgumentException>(() => property.SetValue(wifi, value));
-    
-        Assert.Contains($"{property.Name} cannot be null, empty or whitespace", exception.Message);
+        AssertProperty(property, value);
     }
     
     [Theory]
     [MemberData(nameof(PropertyHelper.GetNullableStringProperties), MemberType = typeof(PropertyHelper))]
     public void NullableStringProperty_ShouldThrowArgumentException_WhenIsEmptyOrWhitespace(PropertyInfo property, string value)
     {
-        var wifi = new WifiNetwork();
-        
-        var exception = Assert.Throws<ArgumentException>(() => property.SetValue(wifi, value));
-    
-        Assert.Contains($"{property.Name} cannot be empty or whitespace", exception.Message);
+        AssertProperty(property, value);
     }
     
     [Theory]
     [MemberData(nameof(PropertyHelper.GetNonNullableIntProperties), MemberType = typeof(PropertyHelper))]
     public void NonNullableIntProperty_ShouldThrowArgumentException_WhenValueIsInvalid(PropertyInfo property, int value)
     {
-        var wifi = new WifiNetwork();
-        
-        var exception = Assert.Throws<ArgumentException>(() => property.SetValue(wifi, value));
-    
-        Assert.Contains($"{property.Name} cannot be set to {value}", exception.Message);
+        AssertProperty(property, value);
     }
     
     [Theory]
     [MemberData(nameof(PropertyHelper.GetNullableIntProperties), MemberType = typeof(PropertyHelper))]
     public void NullableIntProperty_ShouldThrowArgumentException_WhenValueIsInvalid(PropertyInfo property, int? value)
+    {
+        AssertProperty(property, value);
+    }
+
+    private void AssertProperty(PropertyInfo property, object value)
     {
         var wifi = new WifiNetwork();
         
