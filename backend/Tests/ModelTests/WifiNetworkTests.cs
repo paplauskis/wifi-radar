7f1581856f4618b7b52556e7baaf46f31e8f23fa
paplauskis <paplauskaskostas@gmail.com>
2025-05-15 13:53:34 +0300

    moved property retrieval methods to PropertyHelper class

--- backend/Tests/ModelTests/WifiNetworkTests.cs
+++ backend/Tests/ModelTests/WifiNetworkTests.cs
@@ -1,13 +1,14 @@
 using System.Reflection;
 using API.Domain.Models;
 using Xunit;
+using Tests.Helpers;
 
 namespace Tests.ModelTests;
 
 public class WifiNetworkTests
 {
     [Theory]
-    [MemberData(nameof(GetNonNullableStringProperties))]
+    [MemberData(nameof(PropertyHelper.GetNonNullableStringProperties), MemberType = typeof(PropertyHelper))]
     public void NonNullableStringProperty_ShouldThrowArgumentException_WhenIsEmptyOrWhitespace(PropertyInfo property, string value)
     {
         var wifi = new WifiNetwork();
@@ -18,7 +19,7 @@ public class WifiNetworkTests
     }
     
     [Theory]
-    [MemberData(nameof(GetNullableStringProperties))]
+    [MemberData(nameof(PropertyHelper.GetNullableStringProperties), MemberType = typeof(PropertyHelper))]
     public void NullableStringProperty_ShouldThrowArgumentException_WhenIsEmptyOrWhitespace(PropertyInfo property, string value)
     {
         var wifi = new WifiNetwork();
@@ -26,36 +27,5 @@ public class WifiNetworkTests
         var exception = Assert.Throws<ArgumentException>(() => property.SetValue(wifi, value));
     
         Assert.Contains($"{property.Name} cannot be empty or whitespace", exception.Message);
-    }
-
-    private static IEnumerable<object[]> GetProperties<T>(bool isNullable, T[] testValues)
-    {
-        var valuesToTest = testValues;
-
-        var properties = typeof(WifiNetwork)
-            .GetProperties()
-            .Where(p => p.PropertyType == typeof(T) && IsReferencePropertyNullable(p) == isNullable)
-            .ToList();
-
-        foreach (var prop in properties)
-        {
-            foreach (var value in valuesToTest)
-            {
-                yield return new object[] { prop, value };
-            }
-        }
-    }
-
-    public static IEnumerable<object[]> GetNonNullableStringProperties()
-        => GetProperties<string>(false, ["", "   ", null]);
-    
-    public static IEnumerable<object[]> GetNullableStringProperties()
-        => GetProperties<string>(true, ["", "   "]);
-
-    private static bool IsReferencePropertyNullable(PropertyInfo property)
-    {
-        var nullabilityContext = new NullabilityInfoContext();
-        var nullabilityInfo = nullabilityContext.Create(property);
-        return nullabilityInfo.WriteState == NullabilityState.Nullable;
     }
 }
