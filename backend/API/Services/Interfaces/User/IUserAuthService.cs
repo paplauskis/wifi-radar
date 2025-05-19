5b60b1dd8f430372bb92b522acd7d078b4eefaaa
paplauskis <paplauskaskostas@gmail.com>
2025-05-19 21:36:26 +0300

    added method signatures

--- backend/API/Services/Interfaces/User/IUserAuthService.cs
+++ backend/API/Services/Interfaces/User/IUserAuthService.cs
@@ -1,6 +1,9 @@
+using API.Domain;
+
 namespace API.Services.Interfaces.User;
 
 public interface IUserAuthService
 {
-    
+    Task<UserLoginResponseDto> HandleUserLogin(UserLoginRequestDto userRequestDto);
+    Task<UserLoginResponseDto> HandleUserRegistration(UserLoginRequestDto userRequestDto);
 }
--- backend/API/Services/Interfaces/User/IUserFavoriteService.cs
+++ backend/API/Services/Interfaces/User/IUserFavoriteService.cs
@@ -1,6 +1,10 @@
+using API.Domain.Models;
+
 namespace API.Services.Interfaces.User;
 
 public interface IUserFavoriteService
 {
-    
+    Task<List<WifiNetwork>> GetUserFavoritesAsync(string userId);
+    Task<WifiNetwork> AddUserFavoriteAsync(string userId, WifiNetwork wifi);
+    Task DeleteUserFavoriteAsync(string userId, string wifiId);
 }
