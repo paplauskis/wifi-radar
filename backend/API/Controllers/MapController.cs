beda2ccdfdb0d5c56b420abe2aaf48371b786622
paplauskis <paplauskaskostas@gmail.com>
2025-05-20 17:42:58 +0300

    added search endpoint

--- backend/API/Controllers/MapController.cs
+++ backend/API/Controllers/MapController.cs
@@ -1,3 +1,4 @@
+using API.Services.Interfaces.Map;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -6,8 +7,24 @@ namespace API.Controllers;
 [Route("/api/map")]
 public class MapController : ControllerBase
 {
-    public MapController()
+    private readonly IMapService _mapService;
+
+    public MapController(IMapService mapService)
     {
-        
+        _mapService = mapService;
+    }
+    
+    [HttpGet("/search")]
+    public async Task<IActionResult> Search([FromQuery] string city, [FromQuery] int? radius) //radius in meters
+    {
+        try
+        {
+            var wifis = await _mapService.Search(city, radius); 
+            return Ok(wifis);
+        }
+        catch (Exception e) // specific exception handling will be implemented later
+        {
+            return BadRequest(e.Message);
+        }
     }
 }
