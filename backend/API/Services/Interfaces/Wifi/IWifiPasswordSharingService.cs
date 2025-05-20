a8ab3549992ed394664bb9a9a0aee722bcb56cbe
paplauskis <paplauskaskostas@gmail.com>
2025-05-20 17:44:25 +0300

    add two methods

--- backend/API/Services/Interfaces/Wifi/IWifiPasswordSharingService.cs
+++ backend/API/Services/Interfaces/Wifi/IWifiPasswordSharingService.cs
@@ -1,6 +1,9 @@
+using API.Domain;
+
 namespace API.Services.Interfaces.Wifi;
 
 public interface IWifiPasswordSharingService
 {
-    
+    Task<string> AddPasswordAsync(PasswordDto passwordDto);
+    Task<List<string>> GetPasswordsAsync(string wifiId);
 }
