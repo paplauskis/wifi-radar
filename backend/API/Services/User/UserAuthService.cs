dc6612cf13066856a88538b8c4c2ab1199c0c222
paplauskis <paplauskaskostas@gmail.com>
2025-05-22 19:18:12 +0300

    implement HandleUserLogin method

--- backend/API/Services/User/UserAuthService.cs
+++ backend/API/Services/User/UserAuthService.cs
@@ -1,18 +1,35 @@
+using API.Data.Repositories.Interfaces;
+using API.Domain;
 using API.Domain.Dto;
+using API.Helpers;
+using API.Services.Interfaces.Auth;
 using API.Services.Interfaces.User;
 
 namespace API.Services.User;
 
 public class UserAuthService : IUserAuthService
 {
-    public UserAuthService()
+    private readonly IAuthenticatable _jwtService;
+    private readonly IUserRepository _userRepository;
+    private readonly IPasswordHelper _passwordHelper;
+    
+    public UserAuthService(IAuthenticatable jwtService, IUserRepository repo, IPasswordHelper passwordHelper)
     {
-        
+        _jwtService = jwtService;
+        _userRepository = repo;
+        _passwordHelper = passwordHelper;
     }
 
     public async Task<UserLoginResponseDto> HandleUserLogin(UserLoginRequestDto userRequestDto)
     {
-        throw new NotImplementedException();
+        var user = await _jwtService.AuthenticateUser(userRequestDto);
+
+        if (user == null)
+        {
+            throw new UnauthorizedAccessException();
+        }
+
+        return user;
     }
 
     public async Task<UserLoginResponseDto> HandleUserRegistration(UserLoginRequestDto userRequestDto)
