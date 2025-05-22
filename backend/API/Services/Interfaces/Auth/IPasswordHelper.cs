f46a99742176004a8cac5edc9d40e24ac0a2d7ec
paplauskis <paplauskaskostas@gmail.com>
2025-05-22 19:16:12 +0300

    create interface for services that need password hashing functionality

---
+++ backend/API/Services/Interfaces/Auth/IPasswordHelper.cs
@@ -0,0 +1,9 @@
+
+
+namespace API.Services.Interfaces.Auth;
+
+public interface IPasswordHelper
+{
+    string HashPassword(Domain.Models.User user, string password);
+    bool VerifyPassword(Domain.Models.User user, string hashedPassword, string password);
+}
