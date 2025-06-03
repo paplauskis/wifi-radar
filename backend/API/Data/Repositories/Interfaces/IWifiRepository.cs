27fe20b938c0c8d6dc833ce48f05cbde0793c9cb
Šarlotė Volosevič <s053735@ad.viko.lt>
2025-06-03 18:15:44 +0300

    added additional interfaces

--- backend/API/Data/Repositories/Interfaces/IWifiRepository.cs
+++ backend/API/Data/Repositories/Interfaces/IWifiRepository.cs
@@ -4,7 +4,8 @@ namespace API.Data.Repositories.Interfaces;
 
 public interface IWifiRepository : ICrudRepository<WifiNetwork>
 {
+    Task AddPasswordAsync(string wifiId, string password);
     Task<List<WifiNetwork>> GetByCityAsync(string city);
-
+    Task<IEnumerable<object>> GetPasswordsByWifiIdAsync(string wifiId);
     Task UpdatePasswordAsync(string wifiId, string password);
 }
