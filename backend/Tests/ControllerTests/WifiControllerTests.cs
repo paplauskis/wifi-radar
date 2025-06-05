c88ae7db0114e6413fee730d55e39ea370151d1b
paplauskis <paplauskaskostas@gmail.com>
2025-06-05 16:28:06 +0300

    create AddWifiReview_WithValidData_ShouldReturnOk test
    test checks if status code 200 and saved wifi review are returned when adding a review

--- backend/Tests/ControllerTests/WifiControllerTests.cs
+++ backend/Tests/ControllerTests/WifiControllerTests.cs
@@ -3,6 +3,7 @@ using System.Net.Http.Json;
 using System.Text;
 using API.Domain;
 using API.Domain.Dto;
+using API.Domain.Models;
 using Newtonsoft.Json;
 using Tests.Helpers;
 using Xunit;
@@ -20,7 +21,7 @@ public class WifiControllerTests
         await using var factory = new ApiWebApplicationFactory();
         var client = factory.CreateClient();
         
-        await CreateSampleUser(client);
+        var user = await CreateSampleUser(client);
         
         var addReviewContent = new StringContent(JsonConvert.SerializeObject(wifiReviewDto), Encoding.UTF8, "application/json");
         var addWifiReviewResponse = await client.PostAsync($"{ApiUri}/{wifiReviewDto.WifiId}/review", addReviewContent);
@@ -30,12 +31,36 @@ public class WifiControllerTests
         Assert.Equal("Invalid data passed", addWifiReviewResult);
     }
     
+    [Theory]
+    [MemberData(nameof(WifiReviewDtoHelper.ValidWifiReviewDtos), MemberType = typeof(WifiReviewDtoHelper))]
+    public async Task AddWifiReview_WithValidData_ShouldReturnOk(WifiReviewDto wifiReviewDto)
+    {
+        await using var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        var user = await CreateSampleUser(client);
+        wifiReviewDto.UserId = user.Id ?? throw new Exception("User id is null");
+        
+        var addReviewContent = new StringContent(JsonConvert.SerializeObject(wifiReviewDto), Encoding.UTF8, "application/json");
+        var addWifiReviewResponse = await client.PostAsync($"{ApiUri}/{wifiReviewDto.WifiId}/review", addReviewContent);
+        
+        Assert.Equal(HttpStatusCode.OK, addWifiReviewResponse.StatusCode);
+        
+        WifiReview? addWifiReviewResult = await addWifiReviewResponse.Content.ReadFromJsonAsync<WifiReview>();
+        
+        Assert.NotNull(addWifiReviewResult);
+        Assert.Equal(wifiReviewDto.UserId, addWifiReviewResult.UserId);
+        Assert.Equal(wifiReviewDto.WifiId, addWifiReviewResult.WifiId);
+        Assert.Equal(wifiReviewDto.Text, addWifiReviewResult.Text);
+        Assert.Equal(wifiReviewDto.Rating, addWifiReviewResult.Rating);
+        Assert.Equal(HttpStatusCode.OK, addWifiReviewResponse.StatusCode);
+    }
+    
     private async Task<UserLoginResponseDto> CreateSampleUser(HttpClient client)
     {
         var user = new UserLoginRequestDto { Username = "sampleUser", Password = "randomPassword123" };
         var json = JsonConvert.SerializeObject(user);
         var content = new StringContent(json, Encoding.UTF8, "application/json");
-        var response = await client.PostAsync($"{ApiUri}/auth/register", content);
+        var response = await client.PostAsync($"api/user/auth/register", content);
         var userResponse = await response.Content.ReadFromJsonAsync<UserLoginResponseDto>();
         
         return userResponse;
