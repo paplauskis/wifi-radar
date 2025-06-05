58c4120b068f90491c2c27fdb11fd72d4d5886af
paplauskis <paplauskaskostas@gmail.com>
2025-06-05 10:11:20 +0300

    added Id property

--- backend/API/Domain/Dto/UserLoginResponseDto.cs
+++ backend/API/Domain/Dto/UserLoginResponseDto.cs
@@ -2,6 +2,7 @@ namespace API.Domain.Dto;
 
 public class UserLoginResponseDto
 {
+    public string? Id { get; set; }
     public string? Username { get; set; }
     public string? AccessToken { get; set; }
     public int ExpiresIn { get; set; }
