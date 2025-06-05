f13b64183af711267297db64bcbc3d1166e0da61
paplauskis <paplauskaskostas@gmail.com>
2025-06-05 15:53:30 +0300

    create AddWifiReview_WithInvalidData_ShouldReturnBadRequest

--- backend/Tests/ControllerTests/WifiControllerTests.cs
+++ backend/Tests/ControllerTests/WifiControllerTests.cs
@@ -1,11 +1,46 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using API.Domain;
+using API.Domain.Dto;
+using Newtonsoft.Json;
+using Tests.Helpers;
 using Xunit;
 
 namespace Tests.ControllerTests;
 
 public class WifiControllerTests
 {
-    [Fact]
-    public void Test1()
+    private const string ApiUri = "/api/wifi";
+    
+    [Theory]
+    [MemberData(nameof(WifiReviewDtoHelper.InvalidWifiReviewDtos), MemberType = typeof(WifiReviewDtoHelper))]
+    public async Task AddWifiReview_WithInvalidData_ShouldReturnBadRequest(WifiReviewDto wifiReviewDto)
     {
+        await using var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        
+        await CreateSampleUser(client);
+        
+        var addReviewContent = new StringContent(JsonConvert.SerializeObject(wifiReviewDto), Encoding.UTF8, "application/json");
+        var addWifiReviewResponse = await client.PostAsync($"{ApiUri}/{wifiReviewDto.WifiId}/review", addReviewContent);
+        var addWifiReviewResult = await addWifiReviewResponse.Content.ReadAsStringAsync();
+    
+        Assert.Equal(HttpStatusCode.BadRequest, addWifiReviewResponse.StatusCode);
+        Assert.Contains(wifiReviewDto.WifiId!, addWifiReviewResult);
+        Assert.Contains(wifiReviewDto.UserId, addWifiReviewResult);
+        Assert.Contains(wifiReviewDto.Text!, addWifiReviewResult);
+        Assert.Contains(wifiReviewDto.Rating.ToString()!, addWifiReviewResult);
+    }
+    
+    private async Task<UserLoginResponseDto> CreateSampleUser(HttpClient client)
+    {
+        var user = new UserLoginRequestDto { Username = "sampleUser", Password = "randomPassword123" };
+        var json = JsonConvert.SerializeObject(user);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync($"{ApiUri}/auth/register", content);
+        var userResponse = await response.Content.ReadFromJsonAsync<UserLoginResponseDto>();
+        
+        return userResponse;
     }
 }
