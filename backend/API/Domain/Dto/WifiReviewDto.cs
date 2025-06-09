8c4624e36fcb90c42e1840f98c26f467b3c10515
paplauskis <paplauskaskostas@gmail.com>
2025-06-09 14:17:52 +0300

    add three more properties

--- backend/API/Domain/Dto/WifiReviewDto.cs
+++ backend/API/Domain/Dto/WifiReviewDto.cs
@@ -2,7 +2,9 @@ namespace API.Domain;
 
 public class WifiReviewDto
 {
-    public string? WifiId { get; set; }
+    public string? City { get; set; }
+    public string? Street { get; set; }
+    public int? BuildingNumber { get; set; }
     public string UserId { get; set; }
     public int? Rating { get; set; }
     public string? Text { get; set; }
