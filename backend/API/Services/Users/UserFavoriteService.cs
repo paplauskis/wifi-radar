385b4780c764a10a3177a7ba1ca881ce1f5a40fe
paplauskis <paplauskaskostas@gmail.com>
2025-06-09 12:50:30 +0300

    added boilerplate to check if wifi network is already saved by the user
    the logic needs to be implemented later

--- backend/API/Services/Users/UserFavoriteService.cs
+++ backend/API/Services/Users/UserFavoriteService.cs
@@ -50,6 +50,12 @@ namespace API.Services.Users
             if (wifi == null || string.IsNullOrWhiteSpace(wifi.WifiId))
                 wifi.WifiId = ObjectId.GenerateNewId().ToString();
 
+            // should check if a WifiNetwork with the same City, Street and BuildingNumber is already saved by the user
+            // if the WifiNetwork exists, the doesWifiAlreadyExist variable should be true
+            bool doesWifiAlreadyExist = false;
+            if (doesWifiAlreadyExist)
+                throw new WifiNetworkAlreadyExistsException("Wifi network already exists.", wifi);
+
             var user = await _users.Find(u => u.Id == userId).FirstOrDefaultAsync();
             if (user == null)
                 throw new UserNotFoundException(userId);
