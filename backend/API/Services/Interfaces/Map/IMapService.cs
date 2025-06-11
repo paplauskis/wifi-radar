d5e554624a57ac8cd4ef7b6a68f3e4c6c16e0869
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-06-11 18:57:52 +0300

    add GetCoordinates method

--- backend/API/Services/Interfaces/Map/IMapService.cs
+++ backend/API/Services/Interfaces/Map/IMapService.cs
@@ -5,4 +5,5 @@ namespace API.Services.Interfaces.Map;
 public interface IMapService
 { 
     Task<List<OverpassResponseElementDto>> Search(string city, int? radius);
+    Task<CoordinateDto> GetCoordinates(string city, string street, int buildingNumber);
 }
